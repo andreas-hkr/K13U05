7641a61e8ef5efb7cc8a788986cf9cac3b23c314
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-19 13:25:28 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Skriv en metod (toPoint) som omvandlar en polär koordinat till en rektangulär koordinat.
+        //
+        // Punkt = Klass för att representera en punkt mha. rektangulära koordinater
+        // RPunkt = Klass för att representera en punk mha. polära koordinater
+        //
+        // x = r * cos(θ)
+        // y = r * sin(θ)
+
+
+    }
+}
